88951f26eb7a355907b24224148b3d08fc1aeae3
GhettoCornetto <ghettocornetto@gmail.com>
2017-04-07 01:08:38 +0100

    Initial commit - attempting to build class Framework

--- src/poker/RoundOfPoker.java
+++ src/poker/RoundOfPoker.java
@@ -5,8 +5,30 @@ import java.util.*;
 
 /**
  * Created by Andy on 30/03/2017.
-*/
+ */
 
 public class RoundOfPoker {
+	
+	private int pot;
+	
+	// Empty constructor (not sure what to include here)
+	public RoundOfPoker(){
+		
+	}
 
+	// Main function simulates a round of poker
+	public static void main(String[] args) {
+		DeckOfCards deck = new DeckOfCards();
+		
+		for (int shuffleCount = 0; shuffleCount < 2074; shuffleCount++) {
+			deck.shuffle();
+		}
+		
+		HandOfCards hand1 = new HandOfCards(deck);
+		HandOfCards hand2 = new HandOfCards(deck);
+		
+		System.out.println("Hand 1: " + hand1.toString());
+		System.out.println("Hand 2: " + hand2.toString());
+	}
+	
 }
