d7fb718098585397cc6e6029979908f50847149c
14343826 <jonathan.sweeney@ucdconnect.ie>
2017-04-29 19:19:19 +0100

    Added testing card and hand lists as comment to TestHandOfCards

--- src/poker/TestHandOfCards.java
+++ src/poker/TestHandOfCards.java
@@ -48,3 +48,74 @@ public class TestHandOfCards extends TestCase{
         assertEquals("HandOfCards testIsStraightFlush() is returning a false for a correct Straight Flush.", true, test_hand.isStraightFlush());
     }
 }
+
+/*
+//Test Hearts
+        PlayingCard test_AH = new PlayingCard("A", PlayingCard.HEARTS, 1, 14);
+        PlayingCard test_2H = new PlayingCard("2", PlayingCard.HEARTS, 2, 2);
+        PlayingCard test_3H = new PlayingCard("3", PlayingCard.HEARTS, 3, 3);
+        PlayingCard test_4H = new PlayingCard("4", PlayingCard.HEARTS, 4, 4);
+        PlayingCard test_5H = new PlayingCard("5", PlayingCard.HEARTS, 5, 5);
+        PlayingCard test_6H = new PlayingCard("6", PlayingCard.HEARTS, 6, 6);
+        PlayingCard test_9H = new PlayingCard("9", PlayingCard.HEARTS, 9, 9);
+        PlayingCard test_10H = new PlayingCard("10", PlayingCard.HEARTS, 10, 10);
+        PlayingCard test_JH = new PlayingCard("J", PlayingCard.HEARTS, 11, 11);
+        PlayingCard test_QH = new PlayingCard("Q", PlayingCard.HEARTS, 12, 12);
+        PlayingCard test_KH = new PlayingCard("K", PlayingCard.HEARTS, 13, 13);
+
+        //Test Diamonds
+        PlayingCard test_2D = new PlayingCard("2", PlayingCard.DIAMONDS, 2, 2);
+        PlayingCard test_10D = new PlayingCard("10", PlayingCard.DIAMONDS, 10, 10);
+
+        //Test Clubs
+        PlayingCard test_2C = new PlayingCard("2", PlayingCard.CLUBS, 2, 2);
+        PlayingCard test_7C = new PlayingCard("7", PlayingCard.CLUBS, 7, 7);
+        PlayingCard test_8C = new PlayingCard("8", PlayingCard.CLUBS, 8, 8);
+        PlayingCard test_9C = new PlayingCard("9", PlayingCard.CLUBS, 9, 9);
+        PlayingCard test_10C = new PlayingCard("10", PlayingCard.CLUBS, 10, 10);
+
+        //Test Spades
+        PlayingCard test_AS = new PlayingCard("A", PlayingCard.SPADES, 1, 14);
+        PlayingCard test_6S = new PlayingCard("6", PlayingCard.SPADES, 6, 6);
+        PlayingCard test_10S = new PlayingCard("10", PlayingCard.SPADES, 10, 10);
+
+
+//Test hands
+        test_list.add(new ArrayList<>(Arrays.asList("Royal Flush:")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_10H, test_JH, test_KH, test_QH, test_AH)));   //Royal Flush
+        test_list.add(new ArrayList<>(Arrays.asList("Straight Flush (Ace Low):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_AH, test_3H, test_4H, test_2H, test_5H)));    //Straight Flush (Ace Low)
+        test_list.add(new ArrayList<>(Arrays.asList("Four of a Kind:")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_10H, test_JH, test_10S, test_10C, test_10D)));//Four of a Kind
+        test_list.add(new ArrayList<>(Arrays.asList("Full House:")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_10H, test_10C, test_9C, test_9H, test_10S))); //Full House
+        test_list.add(new ArrayList<>(Arrays.asList("Flush:")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_5H, test_9H, test_KH, test_3H, test_AH)));    //Flush
+        test_list.add(new ArrayList<>(Arrays.asList("Straight (Ace High):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_10D, test_AS, test_KH, test_QH, test_JH)));   //Straight (Ace High)
+        test_list.add(new ArrayList<>(Arrays.asList("Straight (Ace Low):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_AS, test_3H, test_4H, test_2D, test_5H)));    //Straight (Ace Low)
+        test_list.add(new ArrayList<>(Arrays.asList("Three of a Kind:")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_10H, test_10C, test_KH, test_QH, test_10S))); //Three of a Kind
+        test_list.add(new ArrayList<>(Arrays.asList("Two Pair:")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_10H, test_9H, test_KH, test_10S, test_9C)));  //Two Pair
+        test_list.add(new ArrayList<>(Arrays.asList("One Pair:")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_2D, test_9H, test_7C, test_QH, test_9C)));    //One Pair
+        test_list.add(new ArrayList<>(Arrays.asList("High Hand:")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_2D, test_9H, test_7C, test_QH, test_AS)));    //High Hand
+        test_list.add(new ArrayList<>(Arrays.asList("HighHand (Ace High):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_2D, test_3H, test_6H, test_AS, test_7C)));    //HighHand
+        test_list.add(new ArrayList<>(Arrays.asList("HighHand (King High):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_JH, test_8C, test_KH, test_7C, test_QH)));   //High Hand
+        test_list.add(new ArrayList<>(Arrays.asList("Flush (Busted Straight):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_2H, test_3H, test_5H, test_6H, test_9H)));    //Flush (Busted Straight)
+        test_list.add(new ArrayList<>(Arrays.asList("Straight (Busted Flush):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_2D, test_3H, test_4H, test_6H, test_5H)));    //Straight (Busted Flush)
+        test_list.add(new ArrayList<>(Arrays.asList("HighHand (Busted Flush):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_KH, test_3H, test_2H, test_6S, test_9H)));    //High Hand (Busted Flush)
+        test_list.add(new ArrayList<>(Arrays.asList("HighHand (Busted Straight):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_2C, test_3H, test_7C, test_6S, test_5H)));    //High Hand (Busted Straight)
+        test_list.add(new ArrayList<>(Arrays.asList("HighHand (No Busted Flush or Straight):")));
+        test_list.add(new ArrayList<>(Arrays.asList(test_JH, test_8C, test_KH, test_10S, test_2H)));   //High Hand
+
+ */
